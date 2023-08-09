e7ff56345946609bee1fbc13044bc02be5e382b9
root <root@KartikeyPC>
2023-08-09 15:20:13 +0530

    Added Admins Routes

--- Server/index.js
+++ Server/index.js
@@ -11,15 +11,19 @@ app.use(express.json());
 
 let USERS = [];
 let BLOGS = [];
+let ADMINS = [];
 
 try {
   USERS = JSON.parse(fs.readFileSync("users.json", "utf8"));
   BLOGS = JSON.parse(fs.readFileSync("blogs.json", "utf8"));
+  ADMINS = JSON.parse(fs.readFileSync("admins.json", "utf8"));
 } catch {
   USERS = [];
   BLOGS = [];
+  ADMINS = [];
 }
 
+// For Users Authentication
 const SECRET_KEY = "MyBloggingPlatform!";
 
 const authenticateJWT = (req, res, next) => {
@@ -38,9 +42,92 @@ const authenticateJWT = (req, res, next) => {
   }
 };
 
+// For Admins Authentication
+const SECRET_FOR_ADMIN = "WeAreAdminsBuddy";
+
+const authenticateAdmins = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, SECRET_FOR_ADMIN, (err, admin) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.admin = admin;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// Admins Signup
+
+app.post("/admin/signup", (req, res) => {
+  const userCreds = req.body;
+  const email = userCreds.email;
+
+  let existingUser = false;
+
+  for (let elem of ADMINS) {
+    if (elem.email === userCreds.email) {
+      existingUser = true;
+      break;
+    }
+  }
+
+  if (existingUser) {
+    res.status(402).send("Admin already exists with this creds!");
+  } else {
+    let newAdmin = {
+      name: userCreds.name,
+      email: userCreds.email,
+      password: userCreds.password,
+    };
+
+    const token = jwt.sign({ email, role: "admin" }, SECRET_FOR_ADMIN, {
+      expiresIn: "1h",
+    });
+
+    ADMINS.push(newAdmin);
+    fs.writeFileSync("admins.json", JSON.stringify(ADMINS));
+
+    res
+      .status(200)
+      .json({ message: "Admin has been created successfully!", token });
+  }
+});
+
+// Admins Login
+app.post("/admin/login", (req, res) => {
+  const userCreds = req.body;
+  const email = userCreds.email;
+
+  for (let elem of ADMINS) {
+    if (
+      elem.email === userCreds.email &&
+      elem.password === userCreds.password
+    ) {
+      const token = jwt.sign({ email, role: "admin" }, SECRET_FOR_ADMIN, {
+        expiresIn: "1h",
+      });
+      res.status(200).json({ message: "Logged in Successfullly", token });
+    }
+  }
+
+  res.status(403).send("Invalid Credentials!");
+});
+
+// Get All Users
+app.get("/admin/allusers", authenticateAdmins, (req, res) => {
+  res.status(200).send(JSON.stringify(USERS));
+});
+
 // Users Signup Route
 
-app.post("/signup", (req, res) => {
+app.post("/users/signup", (req, res) => {
   const userCreds = req.body;
   let email = userCreds.email;
 
@@ -76,7 +163,7 @@ app.post("/signup", (req, res) => {
 
 // Login route
 
-app.post("/login", (req, res) => {
+app.post("/users/login", (req, res) => {
   const userCreds = req.body;
   let email = userCreds.email;
   for (let elem of USERS) {
@@ -96,7 +183,7 @@ app.post("/login", (req, res) => {
 
 // Route for creating Blog!
 
-app.post("/craft", authenticateJWT, (req, res) => {
+app.post("/users/craft", authenticateJWT, (req, res) => {
   const email = req.headers.email;
   const blogBody = req.body;
 
@@ -129,7 +216,7 @@ app.post("/craft", authenticateJWT, (req, res) => {
 
 // Get All Blogs
 
-app.get("/blogs", authenticateJWT, (req, res) => {
+app.get("/users/blogs", authenticateJWT, (req, res) => {
   res.status(200).send(JSON.stringify(BLOGS));
 });
 
